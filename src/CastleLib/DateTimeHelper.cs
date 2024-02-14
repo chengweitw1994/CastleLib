8bbc93fc775e205b5702944822d39448bb22db18
ChengWei <chengwei.tw.1994@gmail.com>
2024-02-14 22:55:46 +0800

    完成 method: DateTimeHelper.GetDates(DateTime startDate, DateTime endDate, DayOfWeek dayOfWeek) 並通過測試

--- src/CastleLib/DateTimeHelper.cs
+++ src/CastleLib/DateTimeHelper.cs
@@ -12,7 +12,22 @@
         /// <exception cref="NotImplementedException"></exception>
         public static IEnumerable<DateTime> GetDates(DateTime startDate, DateTime endDate, DayOfWeek dayOfWeek)
         {
-            throw new NotImplementedException();
+            if (startDate > endDate) return Enumerable.Empty<DateTime>();
+
+            startDate = startDate.Date;
+            endDate = endDate.Date;
+
+            var result = new List<DateTime>();
+            do
+            {
+                if (startDate.DayOfWeek == dayOfWeek)
+                    result.Add(startDate);
+
+                startDate = startDate.AddDays(1);
+            }
+            while (startDate <= endDate);
+
+            return result;
         }
     }
 }
