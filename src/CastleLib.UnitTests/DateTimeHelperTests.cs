112221204c6481febda1e29fee135afc6abad3a4
ChengWei <chengwei.tw.1994@gmail.com>
2024-02-14 20:15:10 +0800

    新增 class: DateTimeHelper DateTimeHelper 新增 method: GetDates(DateTime startDate, DateTime endDate, DayOfWeek dayOfWeek) 新增 class: DateTimeHelperTests

---
+++ src/CastleLib.UnitTests/DateTimeHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+namespace CastleLib.UnitTests
+{
+    [TestClass]
+    public class DateTimeHelperTests
+    {
+        [TestMethod]
+        public void GetDatesTest_20240214至20240214的禮拜三_回傳20240214()
+        {
+            // arrange
+            var startDate = new DateTime(2024, 2, 14);
+            var endDate = new DateTime(2024, 2, 14);
+            var dayOfWeek = DayOfWeek.Wednesday;
+            var expected = new List<DateTime>()
+            {
+                new(2024, 2, 14)
+            };
+
+            // act
+            var actual = DateTimeHelper.GetDates(startDate, endDate, dayOfWeek);
+
+            // assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/CastleLib/DateTimeHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace CastleLib
+{
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// 回傳期間內指定的日子
+        /// </summary>
+        /// <param name="startDate">起</param>
+        /// <param name="endDate">迄</param>
+        /// <param name="dayOfWeek">指定星期幾</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static IEnumerable<DateTime> GetDates(DateTime startDate, DateTime endDate, DayOfWeek dayOfWeek)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
