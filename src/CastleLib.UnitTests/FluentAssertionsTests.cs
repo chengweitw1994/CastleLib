9bb38c0ac1816bed6909567914e0117bfd20e284
ChengWei <chengwei.tw.1994@gmail.com>
2024-02-14 20:10:04 +0800

    新增 FluentAssertions 套件

---
+++ src/CastleLib.UnitTests/FluentAssertionsTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+
+namespace CastleLib.UnitTests
+{
+    [TestClass]
+    public class FluentAssertionsTests
+    {
+        [TestMethod]
+        public void BeEquivalentToTest_比較有相同value但不同instance的兩個集合()
+        {
+            // arrange
+            var expected = new List<DateTime>
+            {
+                new(2024, 2, 13),
+                new(2024, 2, 14)
+            };
+
+            // act
+            var actual = new List<DateTime>
+            {
+                new(2024, 2, 14),
+                new(2024, 2, 13)
+            };
+
+            // assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
